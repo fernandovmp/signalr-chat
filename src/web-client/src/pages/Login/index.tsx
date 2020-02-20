fb1e27eddf51a49eb887338ca860f0027bbdc060
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-02-20 16:06:14 -0300

    Add login page with username input field

---
+++ src/web-client/src/pages/Login/index.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import useLocalStorage from '../../hooks/useLocalStorage';
+import './styles.css';
+
+const LoginPage: React.FC = () => {
+    const [username, setUsername] = useLocalStorage<string>('username', '');
+    const [usernameInputValue, setUsernameInputValue] = useState<string>(
+        username
+    );
+    const history = useHistory();
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setUsername(usernameInputValue);
+        history.push('/chat');
+    };
+
+    return (
+        <form className="login-form" onSubmit={handleSubmit}>
+            <label>
+                Username
+                <input
+                    value={usernameInputValue}
+                    onChange={e => setUsernameInputValue(e.target.value)}
+                />
+            </label>
+            <button type="submit">ENTRAR</button>
+        </form>
+    );
+};
+
+export default LoginPage;
