4a298691e406e758e307344b7529f80d6c30da47
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-02-23 15:17:03 -0300

    Add send message functionality

--- src/web-client/src/pages/Chat/index.tsx
+++ src/web-client/src/pages/Chat/index.tsx
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useLocalStorage from '../../hooks/useLocalStorage';
 import './styles.css';
+import Message from '../../models/Message';
+import MessageBalloon from '../../components/MessageBalloon';
 
 const ChatPage: React.FC = () => {
     const [username] = useLocalStorage('username', '');
+    const [messages, setMessages] = useState<Message[]>([]);
+    const [messageInputValue, setMessageInputValue] = useState('');
+
+    const handleSend = async () => {
+        const message: Message = {
+            sender: username,
+            content: messageInputValue,
+            date: new Date()
+        };
+        setMessages(previousState => [...previousState, message]);
+        setMessageInputValue('');
+    };
 
     return (
         <div className="chat-container">
-            <div className="chat-messages"></div>
+            <div className="chat-messages">
+                {messages.map(_message => (
+                    <MessageBalloon
+                        key={`${_message.date}-${_message.sender}`}
+                        message={_message}
+                    />
+                ))}
+            </div>
             <div className="chat-input-area">
                 <textarea
                     placeholder="Digite sua menssagem..."
+                    value={messageInputValue}
+                    onChange={e => setMessageInputValue(e.target.value)}
                     maxLength={255}
                 ></textarea>
-                <button>
+                <button onClick={() => handleSend()}>
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         width="24"
