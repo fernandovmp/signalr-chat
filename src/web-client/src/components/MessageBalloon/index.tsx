c9801908b70abeddd19f953307813916a98fdb15
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-02-23 15:16:09 -0300

    Add message balloon component

---
+++ src/web-client/src/components/MessageBalloon/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Message from '../../models/Message';
+import './styles.css';
+
+interface MessageBalloonProps {
+    message: Message;
+}
+
+const MessageBalloon: React.FC<MessageBalloonProps> = ({ message }) => {
+    return (
+        <div className="message-balloon">
+            <strong>{message.sender}</strong>
+            <p className="message">{message.content}</p>
+            <p className="message-date">{message.date.toString()}</p>
+        </div>
+    );
+};
+
+export default MessageBalloon;
