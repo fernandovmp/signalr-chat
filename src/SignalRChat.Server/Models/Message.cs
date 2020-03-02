a16245bdff6f18d52d3c13535262affceaee0a5f
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-03-02 18:09:18 -0300

    Add Message model to backend

---
+++ src/SignalRChat.Server/Models/Message.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SignalRChat.Server.Models
+{
+    public class Message
+    {
+        public string Sender { get; set; }
+        public string Content { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
