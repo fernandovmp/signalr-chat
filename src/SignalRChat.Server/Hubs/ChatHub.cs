f95732ba946259c75742dfcbb18a97e13df587f0
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-03-15 17:17:09 -0300

    Add join chat message to chat hub

--- src/SignalRChat.Server/Hubs/ChatHub.cs
+++ src/SignalRChat.Server/Hubs/ChatHub.cs
@@ -6,6 +6,11 @@ namespace SignalRChat.Server.Hubs
 {
     public class ChatHub : Hub
     {
+        public async Task JoinChat(string username)
+        {
+            await Clients.Others.SendAsync("UserJoined", username);
+        }
+
         public async Task SendMessage(Message message)
         {
             await Clients.All.SendAsync("ReceiveMessage", message);
