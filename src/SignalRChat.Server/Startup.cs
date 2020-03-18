e5529dc66c10e56d05397974332864b763ac60b6
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-03-18 18:44:00 -0300

    Add IDbConnection and IUserRepository to dependency injection

--- src/SignalRChat.Server/Startup.cs
+++ src/SignalRChat.Server/Startup.cs
@@ -1,22 +1,35 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SignalRChat.Server.Hubs;
+using SignalRChat.Server.Repositories;
 
 namespace SignalRChat.Server
 {
     public class Startup
     {
+        private IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IDbConnection>(sp =>
+                new SqlConnection(Configuration.GetConnectionString("ConnectionString")));
+            services.AddScoped<IUserRepository, UserRepository>();
+
             services.AddCors();
             services.AddSignalR();
         }
