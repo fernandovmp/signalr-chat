beea20c6c9904b4d5da69e4ba3bc07309fd9e97d
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-04-01 18:00:01 -0300

    Create error view model

---
+++ src/SignalRChat.Server/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace SignalRChat.Server.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string Message { get; set; }
+        public List<object> Errors { get; set; }
+    }
+}
