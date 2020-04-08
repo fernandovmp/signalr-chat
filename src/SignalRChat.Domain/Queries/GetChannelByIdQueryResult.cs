4405a62265eb745a43a3371e248f6c542ddf69b9
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-04-08 13:25:15 -0300

    Add description property in GetChannelByIdQueryResult

--- src/SignalRChat.Domain/Queries/GetChannelByIdQueryResult.cs
+++ src/SignalRChat.Domain/Queries/GetChannelByIdQueryResult.cs
@@ -6,6 +6,7 @@ namespace SignalRChat.Domain.Queries
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
         public Guid AdministratorId { get; set; }
     }
 }
