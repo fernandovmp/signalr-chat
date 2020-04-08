7c06f59ae1393b737953803e46e28a1fff305cc8
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-04-08 14:07:11 -0300

    Create data type user output in command results

---
+++ src/SignalRChat.Domain/DataOutputs/UserOutput.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace SignalRChat.Domain.DataOutputs
+{
+    public class UserOutput
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+    }
+}
