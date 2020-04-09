37470b247127b56056e617ea768d5a7c332612fe
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-04-09 16:45:03 -0300

    Delete obsolet sql script

--- sql/create_tables.sql
+++
@@ -1,6 +0,0 @@
-create table users(
-    id UNIQUEIDENTIFIER PRIMARY KEY,
-    username VARCHAR(32) NOT NULL UNIQUE,
-    createdOn datetime2(2) DEFAULT SYSDATETIME(),
-    modifiedOn datetime2(2) DEFAULT SYSDATETIME()
-)
