9020fc0115553207977ab7b08dbeae0a58a49eec
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-04-15 12:06:09 -0300

    Use create style function in login page styles

--- src/web-client/src/pages/Login/styles.ts
+++ src/web-client/src/pages/Login/styles.ts
@@ -1,6 +1,7 @@
 import { mergeStyleSets } from '@uifabric/styling';
 import { memoizeFunction } from '@uifabric/utilities';
 import { CustomStyleSet } from '../../utils/CustomStyleSetType';
+import { cretaeStyle } from '../../styles/createStyle';
 
 export interface ILoginClassNames {
     loginForm: string;
@@ -9,45 +10,40 @@ export interface ILoginClassNames {
     loginFormButton: string;
 }
 
-export const getClassNames = memoizeFunction(
-    (): ILoginClassNames => {
-        const styles: CustomStyleSet<ILoginClassNames> = {
-            loginForm: {
-                display: 'flex',
-                flexDirection: 'column',
-                margin: '50px 30%',
+export const getClassNames = cretaeStyle<ILoginClassNames>({
+    loginForm: {
+        display: 'flex',
+        flexDirection: 'column',
+        margin: '50px 30%',
+    },
+    loginFormLabel: {
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    loginFormInput: {
+        border: 0,
+        borderBottom: 'solid darkorchid 2px',
+        backgroundColor: '#eee',
+        padding: 4,
+        height: 20,
+        selectors: {
+            ':focus': {
+                outline: 'none',
             },
-            loginFormLabel: {
-                display: 'flex',
-                flexDirection: 'column',
+        },
+    },
+    loginFormButton: {
+        border: 'none',
+        borderRadius: 2,
+        backgroundColor: 'darkorchid',
+        color: 'white',
+        height: 40,
+        fontWeight: 'bold',
+        marginTop: 10,
+        selectors: {
+            ':hover': {
+                cursor: 'pointer',
             },
-            loginFormInput: {
-                border: 0,
-                borderBottom: 'solid darkorchid 2px',
-                backgroundColor: '#eee',
-                padding: 4,
-                height: 20,
-                selectors: {
-                    ':focus': {
-                        outline: 'none',
-                    },
-                },
-            },
-            loginFormButton: {
-                border: 'none',
-                borderRadius: 2,
-                backgroundColor: 'darkorchid',
-                color: 'white',
-                height: 40,
-                fontWeight: 'bold',
-                marginTop: 10,
-                selectors: {
-                    ':hover': {
-                        cursor: 'pointer',
-                    },
-                },
-            },
-        };
-        return mergeStyleSets(styles);
-    }
-);
+        },
+    },
+});
