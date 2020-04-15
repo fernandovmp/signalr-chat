af1dc06b2f4450eef6cf6862ed6eb4f4d94a6e1a
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-04-15 12:05:46 -0300

    Add createStyle function

---
+++ src/web-client/src/styles/createStyle.ts
@@ -0,0 +1,7 @@
+import { mergeStyleSets, IStyle } from '@uifabric/styling';
+import { memoizeFunction } from '@uifabric/utilities';
+import { CustomStyleSet } from '../utils/CustomStyleSetType';
+
+export function cretaeStyle<T>(styles: CustomStyleSet<T>) {
+    return memoizeFunction(() => mergeStyleSets(styles));
+}
