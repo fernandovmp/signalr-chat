18eaa5aeadc41fb666af1bc63632f6890d55f537
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-04-18 14:52:00 -0300

    Remove join notification export

--- src/web-client/src/components/index.ts
+++ src/web-client/src/components/index.ts
@@ -1,6 +1,5 @@
 export * from './ChannelItem';
 export * from './ChannelsBar';
 export * from './ChatComponent';
-export * from './JoinNotification';
 export * from './Sidebar';
 export * from './ChatItem';
