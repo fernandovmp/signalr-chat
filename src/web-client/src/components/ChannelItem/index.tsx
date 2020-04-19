276fe370d68725f3423954a6aa5425a3a3da828d
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-04-19 19:15:31 -0300

    Add button to join in channel

--- src/web-client/src/components/ChannelItem/index.tsx
+++ src/web-client/src/components/ChannelItem/index.tsx
@@ -1,22 +1,39 @@
 import React from 'react';
 import Channel from '../../models/Channel';
+import { useChatApiService } from '../../hooks/useChatApiService';
+import useLocalStorage from '../../hooks/useLocalStorage';
+import User from '../../models/User';
 import { getChannelItemStyles } from './styles';
 
 type propsType = {
     channel: Channel;
-    onClick?: (channel: Channel) => void;
 };
 
-export const ChannelItem: React.FC<propsType> = ({ channel, onClick }) => {
-    const { channelItem } = getChannelItemStyles();
-    const handleOnClick = () => {
-        if (!onClick) return;
-        onClick(channel);
+export const ChannelItem: React.FC<propsType> = ({ channel }) => {
+    const chatApiService = useChatApiService();
+    const [user] = useLocalStorage<User>('user', {
+        id: '',
+        username: '',
+    });
+    const {
+        channelItem,
+        descriptionSection,
+        joinButton,
+    } = getChannelItemStyles();
+
+    const handleJoin = async () => {
+        await chatApiService.joinChannel(channel, user);
     };
+
     return (
-        <div className={channelItem} onClick={handleOnClick}>
-            <strong>{channel.name}</strong>
-            <small>{channel.description ?? 'No description'}</small>
+        <div className={channelItem}>
+            <section className={descriptionSection}>
+                <strong>{channel.name}</strong>
+                <small>{channel.description ?? 'No description'}</small>
+            </section>
+            <button className={joinButton} onClick={handleJoin}>
+                Join
+            </button>
         </div>
     );
 };
--- src/web-client/src/components/ChannelItem/styles.ts
+++ src/web-client/src/components/ChannelItem/styles.ts
@@ -2,20 +2,45 @@ import { cretaeStyle } from '../../styles/createStyle';
 
 export interface IChannelItemClassNames {
     channelItem: string;
+    descriptionSection: string;
+    joinButton: string;
 }
 
 export const getChannelItemStyles = cretaeStyle<IChannelItemClassNames>({
     channelItem: {
         margin: '0 10px',
         padding: 10,
+        display: 'flex',
+        flexDirection: 'row',
+        border: '1px solid #b700ff',
+        borderRadius: 4,
+    },
+    descriptionSection: {
         fontSize: 'medium',
         display: 'flex',
         flexDirection: 'column',
-        border: '1px solid #b700ff',
-        borderRadius: 4,
+        flexGrow: 1,
         selectors: {
             '> small': {
                 margin: '10px 0',
+                wordWrap: 'break-word',
+            },
+            '> strong': {
+                wordWrap: 'break-word',
+            },
+        },
+    },
+    joinButton: {
+        border: 'none',
+        borderRadius: 4,
+        backgroundColor: '#b700ff',
+        color: 'white',
+        height: 30,
+        width: 50,
+        fontWeight: 'bold',
+        selectors: {
+            ':hover': {
+                cursor: 'pointer',
             },
         },
     },
