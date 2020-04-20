c1e52eeb9e3333a79abc1ec6a96e16b54fc2d8a1
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-04-20 16:12:11 -0300

    Configure routes

--- src/web-client/src/App.tsx
+++ src/web-client/src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import LoginPage from './pages/Login';
 import ChatPage from './pages/Chat';
 import { ChatApiService, ChatService } from './services';
+import PageTemplate from './pages/PageTemplate';
+import { routes } from './routes';
 
 const chatService = new ChatService('https://localhost:5001/chatHub');
 const chatApiService = new ChatApiService('https://localhost:5001/api');
@@ -10,15 +12,14 @@ const chatApiService = new ChatApiService('https://localhost:5001/api');
 function App() {
     return (
         <BrowserRouter>
-            <Route path="/" exact={true}>
-                <LoginPage chatApiService={chatApiService} />
-            </Route>
-            <Route path="/chat" exact={true}>
-                <ChatPage
-                    chatService={chatService}
-                    chatApiService={chatApiService}
-                />
-            </Route>
+            <Switch>
+                <Route path="/" exact={true}>
+                    <LoginPage chatApiService={chatApiService} />
+                </Route>
+                <Route path="/">
+                    <PageTemplate routes={routes} />
+                </Route>
+            </Switch>
         </BrowserRouter>
     );
 }
