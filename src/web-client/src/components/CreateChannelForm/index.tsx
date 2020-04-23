0cd4c604361a34fa8c426f3c5954ce1c7b0c4729
Fernando Mariano Pereira <fernandovmp@outlook.com>
2020-04-23 14:37:41 -0300

    Add validation in create channel form

--- src/web-client/src/components/CreateChannelForm/index.tsx
+++ src/web-client/src/components/CreateChannelForm/index.tsx
@@ -6,6 +6,9 @@ import { getCreateChannelFormStyles } from './styles';
 import { useChatApiService } from '../../hooks/useChatApiService';
 import { useHistory } from 'react-router-dom';
 import Channel from '../../models/Channel';
+import { InputField } from '../InputField';
+import ErrorModel from '../../models/ErrorModel';
+import { ErrorBox } from '../ErrorBox';
 
 export const CreateChannelForm: React.FC = () => {
     const [channelName, setChannelName] = useState('');
@@ -14,18 +17,58 @@ export const CreateChannelForm: React.FC = () => {
         id: '',
         username: '',
     });
+    const [error, setError] = useState<ErrorModel>();
     const chatApiService = useChatApiService();
     const history = useHistory();
-    const { formButton, formInput, formLabel } = getCommonStyles();
+    const { formButton } = getCommonStyles();
     const { createChannelForm } = getCreateChannelFormStyles();
+
+    const handleValidations = () => {
+        let valid = true;
+        const inputErrors = [];
+        if (channelName.trim() === '') {
+            inputErrors.push({
+                property: 'Name',
+                message: "Channel name can't be null or white spaces",
+            });
+            valid = false;
+        }
+        if (channelName.trim().length > 32) {
+            inputErrors.push({
+                property: 'Name',
+                message: 'Channel name should be at maximum 32 characters',
+            });
+            valid = false;
+        }
+        if (channelDescription.length > 100) {
+            inputErrors.push({
+                property: 'Description',
+                message:
+                    'Channel description should be at maximum 100 characters',
+            });
+        }
+        if (!valid) {
+            setError({
+                message: 'Invalid input',
+                errors: inputErrors,
+            });
+        }
+        return valid;
+    };
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        if (!handleValidations()) return;
         const response = await chatApiService.createChannelAsync(
             channelName,
             channelDescription,
             user.id
         );
+        const errors = response as ErrorModel;
+        if (errors.message) {
+            setError(errors);
+            return;
+        }
         const channel = response as Channel;
         if (channel.id) {
             history.push(`/chat/${channel.id}`);
@@ -34,22 +77,28 @@ export const CreateChannelForm: React.FC = () => {
 
     return (
         <form className={createChannelForm} onSubmit={handleSubmit}>
-            <label className={formLabel}>
-                Channel name:
-                <input
-                    className={formInput}
-                    value={channelName}
-                    onChange={(e) => setChannelName(e.target.value)}
-                />
-            </label>
-            <label className={formLabel}>
-                Channel description:
-                <input
-                    className={formInput}
-                    value={channelDescription}
-                    onChange={(e) => setChannelDescription(e.target.value)}
-                />
-            </label>
+            {error !== undefined && <ErrorBox message={error.message} />}
+            <InputField
+                fieldLabel="Channel name:"
+                value={channelName}
+                onChange={setChannelName}
+                errors={error?.errors
+                    ?.filter(
+                        (propertyError) => propertyError.property === 'Name'
+                    )
+                    .map((propertyError) => propertyError.message)}
+            />
+            <InputField
+                fieldLabel="Channel description:"
+                value={channelDescription}
+                onChange={setChannelDescription}
+                errors={error?.errors
+                    ?.filter(
+                        (propertyError) =>
+                            propertyError.property === 'Description'
+                    )
+                    .map((propertyError) => propertyError.message)}
+            />
             <button className={formButton} type="submit">
                 Create
             </button>
